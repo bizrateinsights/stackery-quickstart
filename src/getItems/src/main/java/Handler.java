de8f19ff25da6bb04e27022b0f2a7e5529f92893
Kurt Muehlner <kmuehlner@bizrate.com>
2019-09-30 15:16:28 -0700

    now it prints hi

--- src/getItems/src/main/java/Handler.java
+++ src/getItems/src/main/java/Handler.java
@@ -5,6 +5,7 @@ public class Handler {
     public Object handler(Object event) {
         Gson gson = new Gson();
         System.out.println(gson.toJson(event));
+	System.out.println('hi');
 
         return new HashMap();
     }
